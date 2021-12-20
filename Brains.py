751c3d64dfaf38e43038c767b4842f2ba92d6db7
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-12-20 14:22:48 -0800

    all buttons set

--- Brains.py
+++ Brains.py
@@ -6,8 +6,17 @@ remote = tkinter.Tk(className = "Remote")
 
 remote_canvas = tkinter.Canvas(remote)
 
-up_img = tkinter.PhotoImage(master = remote_canvas, file = r'btn_up.png')
+up_img = tkinter.PhotoImage(master = remote_canvas, file = r'RemoteAssets/btn_up.png')
 up = tkinter.Button(remote, image = up_img).pack(side = 'top')
+
+left_img = tkinter.PhotoImage(master = remote_canvas, file = r'RemoteAssets/btn_left.png')
+left = tkinter.Button(remote, image = left_img).pack(side = 'left')
+
+right_img = tkinter.PhotoImage(master = remote_canvas, file = r'RemoteAssets/btn_right.png')
+right = tkinter.Button(remote, image = right_img).pack(side = 'right')
+
+down_img = tkinter.PhotoImage(master = remote_canvas, file = r'RemoteAssets/btn_down.png')
+down = tkinter.Button(remote, image = down_img).pack(side = 'bottom')
 
 ##################################################################################
 # Code for TV
